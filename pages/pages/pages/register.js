12c335cab3b405b81aa80ac5abdfadfbf1c6b399
oduorgeorge513-pixel <oduorgeorge513@gmail.com>
2025-12-29 16:13:09 +0300

    Create register.js

---
+++ pages/pages/pages/register.js
@@ -0,0 +1,10 @@
+export default function Register() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Create Account</h2>
+      <input placeholder="Email" /><br /><br />
+      <input type="password" placeholder="Password" /><br /><br />
+      <button>Register</button>
+    </div>
+  );
+}
